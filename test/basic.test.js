37caaeea98b0c13b92e0db5df2f375f7aa8be8c9
extremeheat <extreme@protonmail.ch>
2024-03-29 07:49:35 +0000

    add test

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,13 +1,19 @@
 /* eslint-env mocha */
-globalThis.isMocha = true
+globalThis.isMocha = !process.env.CI
 const lib = require('gh-helpers')
 const assert = require('assert')
 
 describe('basic usage', () => {
-  it('test import', () => {
-    const github = lib()
+  const github = lib()
+
+  it('test import', function () {
     const defBranch = github.getDefaultBranch()
     console.log('Default branch', defBranch)
     assert(defBranch === 'main' || defBranch === 'master')
+  })
+
+  it('artifacts', async function () {
+    const artifacts = await github.artifacts.list()
+    console.log('List of Artifacts on Repo', artifacts)
   })
 })
